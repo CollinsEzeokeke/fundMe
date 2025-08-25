1f74ac7b55c658769762ef7bb2856679f8594058
CollinsEzeokeke <collinsmecosy@gmail.com>
2025-08-25 11:55:02 +0100

    added an agregator interface linking it to a contract address

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe {
     uint256 public minimumUsdt = 5;
 
@@ -8,7 +26,15 @@ contract FundMe {
         require(msg.value >= minimumUsdt, "didn't send enough ETH"); //1e18 = 1 ETH = 1000000000000000000 1 * 10 ** 18
     }
 
-    function getPrice() public  {}
-    
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI 
+        
+    }
+
     function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
