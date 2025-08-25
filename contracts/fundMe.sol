fca7230fb609d089d1fcb5c1ff40ff9cb842795f
CollinsEzeokeke <collinsmecosy@gmail.com>
2025-08-25 11:37:33 +0100

    test

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -7,4 +7,8 @@ contract FundMe {
     function fund() public payable {
         require(msg.value >= minimumUsdt, "didn't send enough ETH"); //1e18 = 1 ETH = 1000000000000000000 1 * 10 ** 18
     }
+
+    function getPrice() public  {}
+    
+    function getConversionRate() public {}
 }
